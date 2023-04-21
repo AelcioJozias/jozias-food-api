7e9402cae97c2e3ef1246037628d87089af94272
AelcioJozias <aelciojoziasputzel18@gmail.com>
2023-04-21 14:04:16 -0300

    arquivos iniciais do projeto

---
+++ src/main/java/com/jozias/food/JoziasFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.jozias.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoziasFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoziasFoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jozias/food/JoziasFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jozias.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoziasFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
