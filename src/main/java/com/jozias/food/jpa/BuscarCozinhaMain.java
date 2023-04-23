b0d8d8b47c4c924000e2e87148e9ba71bd9da967
AelcioJozias <aelciojoziasputzel18@gmail.com>
2023-04-23 14:23:24 -0300

    Buscando cozinha pelo Id

--- src/main/java/com/jozias/food/jpa/BuscarCozinhaMain.java
+++ src/main/java/com/jozias/food/jpa/BuscarCozinhaMain.java
@@ -1,5 +1,7 @@
 package com.jozias.food.jpa;
 
+import java.util.List;
+
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
@@ -7,18 +9,27 @@ import org.springframework.context.ApplicationContext;
 import com.jozias.food.JoziasFoodApiApplication;
 import com.jozias.food.domain.model.Cozinha;
 
-public class ConsultaCozinhaMain {
+public class BuscarCozinhaMain {
 
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new SpringApplicationBuilder(JoziasFoodApiApplication.class)
 				.web(WebApplicationType.NONE)
 				.run(args);
 
-		CadastroCozinha beanCozinha=  applicationContext.getBean(CadastroCozinha.class);
+		CadastroCozinha cadastroCozinha=  applicationContext.getBean(CadastroCozinha.class);
 		
-		Cozinha cozinha = beanCozinha.buscar(2L);
+		List<Cozinha> cozinhas = cadastroCozinha.listar();
 		
-		System.out.println(cozinha.getNome());
+		cozinhas.forEach(e -> System.out.println(e.getNome()));
+		
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Brasileira");
+		
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("Japonesa");
+		
+		cadastroCozinha.adicionar(cozinha1);
+		cadastroCozinha.adicionar(cozinha2);
 		
 	}
 
