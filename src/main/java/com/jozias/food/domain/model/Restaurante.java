c0f9c0e572d90a12abe74da1344802439be09c89
AelcioJozias <aelciojoziasputzel18@gmail.com>
2023-04-24 03:13:35 -0300

    ManyToOne e JoinColumn

--- src/main/java/com/jozias/food/domain/model/Restaurante.java
+++ src/main/java/com/jozias/food/domain/model/Restaurante.java
@@ -5,7 +5,11 @@ import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -16,6 +20,7 @@ public class Restaurante {
 
 	@EqualsAndHashCode.Include
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String nome;
@@ -23,4 +28,7 @@ public class Restaurante {
 	@Column(name = "taxa_frete")
 	private BigDecimal taxaFrete;
 	
+	@ManyToOne
+	@JoinColumn(name = "cozinha_id") //é como se fosse usar o column, mas para a relacao
+	private Cozinha cozinha;
 }
